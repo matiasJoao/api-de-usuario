d66de986aa8838e68de6ef53e03482d886e55dbe
João Victor Matias de Oliveira <joaovictor.a4@gmail.com>
2022-07-21 15:38:36 -0300

    9%

--- Src/Models/user.js
+++ Src/Models/user.js
@@ -1,11 +1,14 @@
 const mongoose = require('../DataBase/index');
+const crypt = require('bcryptjs');
+
+
 
 const userdb = new mongoose.Schema({
-    name : {
+    name: {
         type: String,
         require: true
     },
-    email : {
+    email: {
         type: String,
         require: true,
         unique: true,
@@ -13,18 +16,23 @@ const userdb = new mongoose.Schema({
         
     },
 
-    password : {
+    password: {
         type: String,
         require: true, 
         select: false
     },
 
-    date : {
+    date: {
         type: Date,
         default: Date.now
     }, 
 });
 
+userdb.pre('save',  async function(next){
+    const incript = await crypt.hash(this.password, 10);
+    this.password = incript;
+    next();
+});
 const user = mongoose.model('user', userdb);
 
 module.exports = user;
--- Src/controllers/attControler.js
+++ Src/controllers/attControler.js
@@ -1,15 +1,40 @@
 const express = require ('express');
+const user = require('../Models/user');
 const userdb = require('../Models/user');
 const router = express.Router();
+const crypt = require('bcryptjs');
+const tkn = require('jsonwebtoken');
+const hash = require('../hash/hash.json')
 
 router.post('/register', async (req, res)=> {
+    const {email} = req.body;
     try {
+        if(await userdb.findOne({email})){
+            return res.status(400).send({error: 'Email ja existe'})
+        }
         const user = await userdb.create(req.body);
+        user.password = undefined;
         return res.send({user});
+       
     }
     catch (err){
         return res.status(400).send({error: 'falha no registro'});
     };
 });
+
+router.post('/login', async (req, res)=>{
+    const {email, password} = req.body;
+    const login = await userdb.findOne({email}).select('+password')
+
+    if(!login){
+        return res.status(400).send({error : 'Usuario Nao encontrado'})
+    };
+    if(!await crypt.compare(password, login.password)){
+        return res.status(400).send({error : 'Senha invalida'})
+    }
+    login.password = undefined;
+    const token = tkn.sign({id : login.id}, hash.hash, {expiresIn:86400} )
+    res.send({ login, token});
+})
 
 module.exports = app => app.use('/att', router);
